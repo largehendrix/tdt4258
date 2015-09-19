81a1594f415cec272a8da46c7a4317f95e687208
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2015-09-19 18:23:24 +0200

    kan ting faktisk funke?

--- ex1.s
+++ ex1.s
@@ -96,83 +96,59 @@ _reset:
 	ldr GPIO, =GPIO_BASE
 	ldr CMU, = CMU_BASE
 
-	/* load hfperclken0 */
-	ldr r2, [CMU, #CMU_HFPERCLKEN0]
-
-	/* set gpio bit for clock */
-
-    CMU .req r7
-
-
-	/* Connect registers with correct process */
-	ldr GPIO_LED, =GPIO_PA_BASE
-	ldr GPIO_BTN, =GPIO_PC_BASE
-	ldr GPIO, =GPIO_BASE
-
-	/* Load CMU base */
-	ldr CMU, = CMU_BASE
-
-	/* load hfperclken0 */
-	
-
 	/* set gpio bit */
 	mov r3, #1
 	lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
-    ldr r2, [CMU, #CMU_HFPERCLKEN0]
+	ldr r2, [CMU, #CMU_HFPERCLKEN0]
 	orr r2, r2, r3
-
-	/* store value */
 	str r2, [CMU, #CMU_HFPERCLKEN0]
 	
 
-	/* set high drive strength */
-	ldr r2, = 0x2
-	mov r3, #GPIO_CTRL
-
-	/* set low drive strength */
-    ldr r1, =GPIO_PA_BASE    
-    ldr r2, = 0x3
-	str r2, [r1, #GPIO_CTRL]
+	/* set low drive strength */    
+    	ldr r2, = 0x2
+	str r2, [GPIO_LED, #GPIO_CTRL]
     
-    /* Turn off all LEDs*/
-    //ldr r2, =0xff
-    //lsl r2, r2, #8
-    //str r2, [GPIO, #GPIO_DOUT]
+    	/* Turn off all LEDs*/
+    	//ldr r2, =0xff
+   	//lsl r2, r2, #8
+    	//str r2, [GPIO, #GPIO_DOUT]
 
 	/* set pins 8-15 to output */
-	//ldr r2, = 0x55555555
-	//mov r3, #GPIO_MODEH
-    //add r3, r3, GPIO_LED
-    //str r2, [r3]
+	mov r2, #0x55555555
+	str r2, [GPIO_LED, #GPIO_MODEH]
+
+	mov r2, #0x1
+	lsl r2, r1, #8
+	str r2, [GPIO_LED, #GPIO_DOUT]
 
 	/* set pins 0-7 to input */
 	ldr r2, = 0x33333333
 	mov r3, #GPIO_MODEL
-    add r3, r3, GPIO_BTN
-    str r2, [r3]
+    	add r3, r3, GPIO_BTN
+    	str r2, [r3]
 
-    /* internal pull-up */
-    ldr r2, = 0xff
-    str r2, [GPIO_BTN, #GPIO_DOUT]
+    	/* internal pull-up */
+    	ldr r2, = 0xff
+    	str r2, [GPIO_BTN, #GPIO_DOUT]
 
-    /* enable gpio interrupt */
-    ldr r1, = 0x22222222
-    str r1, [GPIO, #GPIO_EXTIPSELL]
+    	/* enable gpio interrupt */
+    	ldr r1, = 0x22222222
+    	str r1, [GPIO, #GPIO_EXTIPSELL]
 
-    /* enable interrupt on button push*/
-    ldr r1, =0xff
-    str r1, [GPIO, #GPIO_EXTIFALL]   // Enable interrupt on button push
-    // str r1, [GPIO, #GPIO_EXTIRISE] // When enabled will enable interrupt on button release
-    str r1, [GPIO, #GPIO_IEN]
+    	/* enable interrupt on button push*/
+    	ldr r1, =0xff
+    	str r1, [GPIO, #GPIO_EXTIFALL]   // Enable interrupt on button push
+    	// str r1, [GPIO, #GPIO_EXTIRISE] // When enabled will enable interrupt on button release
+    	str r1, [GPIO, #GPIO_IEN]
 
 
 
-    /* enable interrupt handling */
+    	/* enable interrupt handling */
 
-    ldr r1, =0x802
-    ldr r2, =ISER0
-    str r1, [r2]
-    bx lr
+   	ldr r1, =0x802
+    	ldr r2, =ISER0
+    	str r1, [r2]
+    	bx lr
 
 sleep:
 	/* enable sleep */
@@ -198,16 +174,14 @@ gpio_handler:
 	/* Clear interrupt */
 	ldr r1, [GPIO, #GPIO_IF]
 	str r1, [GPIO, #GPIO_IFC]
-    
-    ldr r3, [r2, #GPIO_DIN]     // get input
-    lsl r3, r3, #8              // left shift to get right pins
-    str r3, [r1, #GPIO_DOUT]    // write back to leds
-    bx lr
+    	
+	ldr r3, = GPIO_PA_BASE
+	ldr r4, = GPIO_PC_BASE
 
 	/* Do something when buttons are pressed*/
-	ldr r2 [GPIO_BTN, #GPIO_DIN]
+	ldr r2, [r4, #GPIO_DIN]
 	lsl r2, r2, #8
-	str r2, [GPIO_LED, #GPIO_DOUT]
+	str r2, [r3, #GPIO_DOUT]
 
 	bx lr
 
