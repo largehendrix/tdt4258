960fd3a3fecab85e2c3eaf7cb86e1dc2e5c2cd75
petterrostrup <petterrostrup@gmail.com>
2015-10-01 13:53:43 +0200

    Added interrupt skeleton

--- ex2/interrupt_handlers.c
+++ ex2/interrupt_handlers.c
@@ -2,24 +2,72 @@
 #include <stdbool.h>
 
 #include "efm32gg.h"
+//Include rest of music functions from other c file
 
-/* TIMER1 interrupt handler */
-void __attribute__ ((interrupt)) TIMER1_IRQHandler() 
-{  
-  /*
-    TODO feed new samples to the DAC
-    remember to clear the pending interrupt by writing 1 to TIMER1_IFC
-  */  
+void __attribute__ ((interrupt)) TIMER1_IRQHandler() {
+    /* Clear interrupt flag */
+    *TIMER1_IFC = 1;
+
+    if ( c >= current_note_length ) {
+	c = 0;
+	note_c++;
+    } else {
+	c++;
+    }
+
+    if ( note_c >= current_song->length ) {
+	timer_cleanup();
+	*GPIO_PA_DOUT = 0xFFFF;
+	disableDAC();
+	disableTimer();
+	setupSleep(0b110);
+	return;
+    }
+
+    Note* n = current_song->notes[note_c];
+    int offset = (i % n->num);
+    note(n, offset);
+
+    i++;
 }
 
-/* GPIO even pin interrupt handler */
-void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler() 
-{
-    /* TODO handle button pressed event, remember to clear pending interrupt */
+void GPIO_Handler() {
+    timer_cleanup();
+    GPIO_interrupt_clear();
+    GPIO_LED(); // will stay on until end of song
+
+    int SW = map_input();
+    if (SW == 1) {
+	  //Set up different interrupt events and handle accordingly
+    }
+
+    /* Example of previous users
+    playSong(&JUMP, 0x027F);
+      } else if ( SW == 2) {
+  	playSong(&PEWPEW, 0x3FF);
+      } else if ( SW == 3) {
+  	playSong(&ONEUP, 0x71f);
+      } else if (SW == 4) {
+  	playSong(&THATSNOMOON, 0x3FFF);
+      } else if (SW == 5) {
+  	playSong(&SCOM, 0x24FF);
+      } else if (SW == 6) {
+  	playSong(&CANON, 0x17FF);
+      } else if (SW == 7) {
+  	playSong(&CANON, 0x5FFF);
+      } else if (SW == 8) {
+  	playSong(&CANON, 0xFFF);
+      } else {
+  	playSong(&SCOM, 0x24FF);
+      }
+      */
+
 }
 
-/* GPIO odd pin interrupt handler */
-void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler() 
-{
-    /* TODO handle button pressed event, remember to clear pending interrupt */
+void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler() {
+    GPIO_Handler();
+}
+
+void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler() {
+    GPIO_Handler();
 }
