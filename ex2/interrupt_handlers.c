5f2dde868c3c5ca05190e180d3adc8b8e1834c1b
petterrostrup <petterrostrup@gmail.com>
2015-10-13 20:15:11 +0200

    reverting

--- ex2/interrupt_handlers.c
+++ ex2/interrupt_handlers.c
@@ -2,130 +2,88 @@
 #include <stdbool.h>
 
 #include "efm32gg.h"
-#include "sound.h"
-//Include rest of music functions from other c file
+#include "sounds.h"
 
 extern struct tone *sampleArray;
 extern int songlength;
+extern int runBattlefield;
+
+void disableLowEnergyTimer();
+void disableDAC();
+void playMario();
+
+
+
+/* TIMER1 interrupt handler.
+ * Obselete function. Were used
+ * in previous versions of the
+ * program.
+ *
+ */
+
+void __attribute__ ((interrupt)) TIMER1_IRQHandler()
+{
+	*TIMER1_IFC = 1;
+
+	playMario();
+	playSong(sampleArray, songlength);
+
+}
+
+/* GPIO even pin interrupt handler
+ * This function will call select melody
+ * function which is responseble for
+ * selecting songs. The songs will be
+ * selected based on the button pushed.
+ *
+*/
+
+void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler()
+{
+	/* Clear pending interrupts */
+
+	select_melodies();
+
+	*GPIO_IFC = 0xff;
+	*GPIO_PA_DOUT = (*GPIO_PC_DIN << 8);
+
+
+}
+
+/* GPIO odd pin interrupt handler.
+ * This function will call select melody
+ * function which is responseble for
+ * selecting songs. The songs will be
+ * selected based on the button pushed.
+ *
+ */
+
+void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler()
+{
+	*GPIO_IFC = 0xff;
+	select_melodies();
+	*GPIO_PA_DOUT = (*GPIO_PC_DIN << 8);
+
+}
+
+/* LETIMER0 interrupt handler.
+ * The function will be called
+ * every interrupt pushing
+ * a new sample to the DAC
+ *
+ */
 
 void __attribute__ ((interrupt)) LETIMER0_IRQHandler(){
 
 	*LETIMER0_IFC = 1;
 
+
 	/* Feed new samples to the DAC */
+	if(runBattlefield){
+		play_music(songlength);
+	}else {
 		playSong(sampleArray, songlength);
+	}
 
 
-}
-
-void GPIO_Handler() {
-
-    switch((*GPIO_PC_DIN)){
-  		case 0xfe:
-  			if(timer_running){
-  				disableLowEnergyTimer();
-  				disableDAC();
-  				initSound();
-  			}
-  			timer_running = true;
-  			setupDAC();
-  			setupLowEnergyTimer();
-  			sampleArray=mario;
-  			songlength=215;
-  			break;
-  		case 0xfd:
-  			if(timer_running){
-  				disableLowEnergyTimer();
-  				disableDAC();
-  				initSound();
-  			}
-  			timer_running = true;
-  			setupDAC();
-  			setupLowEnergyTimer();
-  			sampleArray=hit_received;
-  			songlength=36;
-  			break;
-  		case 0xfb:
-  			if(timer_running){
-  				disableLowEnergyTimer();
-  				disableDAC();
-  				initSound();
-  			}
-  			timer_running = true;
-  			setupDAC();
-  			setupLowEnergyTimer();
-  			sampleArray=hit_dealt;
-  			songlength=41;
-  			break;
-  		case 0xf7:
-  			if(timer_running){
-  				disableLowEnergyTimer();
-  				disableDAC();
-  				initSound();
-  			}
-  			timer_running = true;
-  			setupDAC();
-  			setupLowEnergyTimer();
-  			sampleArray=shoot;
-  			songlength=15;
-  			break;
-  		case 0xef:
-  			if(timer_running){
-  				disableLowEnergyTimer();
-  				disableDAC();
-  				initSound();
-  			}
-  			timer_running = true;
-  			runBattlefield = 1;
-  			setupDAC();
-  			setupLowEnergyTimer();
-  			changeTopCounter(8000);
-  			songlength=396000;
-  			break;
-  		case 0xdf:
-  			if(timer_running){
-  				disableLowEnergyTimer();
-  				disableDAC();
-  				initSound();
-  			}
-  			timer_running = true;
-  			setupDAC();
-  			setupLowEnergyTimer();
-  			sampleArray=beep1;
-  			songlength=1;
-  			break;
-  		case 0xbf:
-  			if(timer_running){
-  				disableLowEnergyTimer();
-  				disableDAC();
-  				initSound();
-  			}
-  			timer_running = true;
-  			setupDAC();
-  			setupLowEnergyTimer();
-  			sampleArray=beep2;
-  			songlength=1;
-  			break;
-  		case 0x7f:
-  			if(timer_running){
-  				disableLowEnergyTimer();
-  				disableDAC();
-  				initSound();
-  			}
-  			timer_running = true;
-  			setupDAC();
-  			setupLowEnergyTimer();
-  			sampleArray=beep3;
-  			songlength=1;
-  			break;
-  	}
-
-}
-
-void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler() {
-    GPIO_Handler();
-}
-
-void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler() {
-    GPIO_Handler();
 }
