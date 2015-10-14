e0162bcf558311e97cbee621e72c135880174fbe
petterrostrup <petterrostrup@gmail.com>
2015-10-14 18:23:13 +0200

    sampling

--- ex2/sounds.c
+++ ex2/sounds.c
@@ -22,14 +22,14 @@ void disableDAC();
 bool timer_running;  		/* Used to control whether the timer is running or not*/
 int duration;        		/* Keeps track of wave length */
 int notes_pos;		 		/* Keeps track of the position in the pre-sampled data */
-int pos;             		/* Keeps track of the position in the cusomized samples */ 
+int pos;             		/* Keeps track of the position in the cusomized samples */
 
 struct tone *sampleArray;   /* The sampleArray pased used in the playSong function */
 int songlength;				/* Sample lenght */
 int runBattlefield;			/* Variable used to run the battlefield intro */
 
 bool iterate=false;         /* Set to true if tone has been played for a specified amount of time */
-int counter=0;				/* Keeps track of time duration of tone */ 
+int counter=0;				/* Keeps track of time duration of tone */
 
 
 /* Set all variables to default */
@@ -50,9 +50,9 @@ Function to play a note for a specified amount of milliseconds
 void playNotes(int note, int time){
 	int cycles = convert_from_ms(time); //Cycle variable corresponds to amount of milliseconds specified in time-variable
 
-	int sampling=PERIOD/note; //Create correct number of samples per waveform based on the number of samples per second 
-	
-	
+	int sampling=PERIOD/note; //Create correct number of samples per waveform based on the number of samples per second
+
+
 	/* if-else to transition between high and low */
 	if(sampling/2>=duration){
 		*DAC0_CH0DATA=2000;
@@ -63,7 +63,7 @@ void playNotes(int note, int time){
 		*DAC0_CH1DATA=-2000;
 	}
 	duration++;
-	
+
 	/* Reset duration if end of period */
 	if(duration>=sampling){
 		duration=0;
@@ -77,29 +77,29 @@ void playNotes(int note, int time){
 
 
 /* Function for creating discrete samples
- * from sawtooth waves. Not actually used 
+ * from sawtooth waves. Not actually used
  * in the main program. Decided on square waves
- * because the sound was better. 
+ * because the sound was better.
  */
 
 void sawtoothWave(int note, int time){
 
 		int sampling=PERIOD/note;
-		
+
 		int slope = (1024)/sampling;
-		
+
 		int y = (slope*duration);
-		
+
 		*DAC0_CH0DATA=y;
 		*DAC0_CH1DATA=y;
-		
+
 		duration++;
 
 		if(duration>=sampling){
 			duration=0;
 		}
 		counter++;
-		
+
 		if(counter==time){
 		   iterate=true;
 		   counter=0;
@@ -109,32 +109,32 @@ void sawtoothWave(int note, int time){
 
 
 /* Function for playing samples at 8000Hz.
- * The function pushes a sample every time it 
- * is called. The samples are staticially 
+ * The function pushes a sample every time it
+ * is called. The samples are staticially
  * linked in sound_data.h. At the end of
- * the sample array the timer and DAC 
- * are disabled. 
+ * the sample array the timer and DAC
+ * are disabled.
  *
  */
 void play_music(int size){
 	int note = (char)sounddata_data[notes_pos];
-	
-	
+
+
 	*DAC0_CH0DATA = (note << 1);
 	*DAC0_CH1DATA = (note << 1);
-	
+
 	if(notes_pos > size){
 		notes_pos = 0;
 		disableLowEnergyTimer();
 		disableDAC();
 		initSound();
 	}
-	
+
 	notes_pos++;
 
 }
 
-/* The function converts milliseconds 
+/* The function converts milliseconds
  * to the corresponding number of cycles.
  *
  */
@@ -144,24 +144,24 @@ int convert_from_ms(int millis){
 }
 
 /* Function for playing samples.
- * Based on the sample array, and 
+ * Based on the sample array, and
  * the current postion in the sample
- * it calls the playNotes() function. 
+ * it calls the playNotes() function.
  * When end of the samples are reached
- * the function disables the timer 
+ * the function disables the timer
  * and the DAC.
  *
  */
 
 void playSong(struct tone melody[], int size){
-	
+
 	playNotes(melody[pos].note, melody[pos].time);
-	
+
 	if(iterate==true){
         pos++;
         iterate=false;
 	}
-	else if(pos >= size){	
+	else if(pos >= size){
 		disableLowEnergyTimer();
 		disableDAC();
 		timer_running = false;
@@ -169,9 +169,9 @@ void playSong(struct tone melody[], int size){
 	 }
 }
 
-/* Test function. 
+/* Test function.
  * Tests the mario into.
- * Not used in the final 
+ * Not used in the final
  * version
  */
 
@@ -186,7 +186,7 @@ void playMario(){
  * The function selects melodies based
  * on the button pushed. For selected
  * song the timer and DAC is enabled.
- * If a song is already playing. It 
+ * If a song is already playing. It
  * interrupt the song by disabling the
  * timer and DAC before it continues
  * selecting the new song.
@@ -194,7 +194,7 @@ void playMario(){
  */
 
 void select_melodies(){
-	
+
 	switch((*GPIO_PC_DIN)){
 		case 0xfe:
 			if(timer_running){
@@ -218,12 +218,12 @@ void select_melodies(){
 			setupDAC();
 			setupLowEnergyTimer();
 			sampleArray=hit_received;
-			songlength=36;			
+			songlength=36;
 			break;
 		case 0xfb:
 			if(timer_running){
 				disableLowEnergyTimer();
-				disableDAC(); 
+				disableDAC();
 				initSound();
 			}
 			timer_running = true;
@@ -238,7 +238,7 @@ void select_melodies(){
 				disableDAC();
 				initSound();
 			}
-			timer_running = true; 
+			timer_running = true;
 			setupDAC();
 			setupLowEnergyTimer();
 			sampleArray=shoot;
@@ -248,20 +248,20 @@ void select_melodies(){
 			if(timer_running){
 				disableLowEnergyTimer();
 				disableDAC();
-				initSound();			
+				initSound();
 			}
 			timer_running = true;
 			runBattlefield = 1;
 			setupDAC();
 			setupLowEnergyTimer();
-			changeTopCounter(8000);
+			changeTopCounter(2000);
 			songlength=396000;
 			break;
 		case 0xdf:
 			if(timer_running){
 				disableLowEnergyTimer();
 				disableDAC();
-				initSound();			
+				initSound();
 			}
 			timer_running = true;
 			setupDAC();
@@ -273,7 +273,7 @@ void select_melodies(){
 			if(timer_running){
 				disableLowEnergyTimer();
 				disableDAC();
-				initSound();			
+				initSound();
 			}
 			timer_running = true;
 			setupDAC();
