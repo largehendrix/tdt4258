eb9e5ededdb7b7552955d3b0e965b5600f5f4b41
Agata Jedryszek <agatajedryszek@gmail.com>
2015-10-15 16:06:19 +0200

    fixed comments

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -3,7 +3,6 @@
 
 #include "efm32gg.h"
 
-
 /*
   TODO calculate the appropriate sample period for the sound wave(s)
   you want to generate. The core clock (which the timer clock is derived
@@ -12,7 +11,6 @@
 */
 /* The period between sound samples, in clock cycles */
 #define SAMPLE_PERIOD 32748
-
 
 /* Declaration of peripheral setup functions */
 void setupTimer(uint32_t period);
@@ -25,24 +23,25 @@ void initSound();
 
 /* Your code will start executing here */
 int main(void) {
-
+  /* Call the peripheral setup functions */
 	initSound(); /* Reset every global variable at reset */
-  	setupGPIO(); /* Configure buttons and LEDs */
+  setupGPIO();
 
-	/* These functions are used called when needed. E.g when we play tunes. See
+	/* These functions are called when needed. E.g when we play tunes. See
 	 * sounds.c:selectMelodies() */
-
 	//setupDAC();
-  	//setupLowEnergyTimer();
-    //setupTimer(48000);
+  //setupLowEnergyTimer();
+  //setupTimer(48000);
 
+  /* Enable interrupt handling */
+ 	setupNVIC();
+ 
+  /* TODO for higher energy efficiency, sleep while waiting for interrupts
+     instead of infinite loop for busy-waiting
+  */
 
- 	setupNVIC(); /*Setup exceptions vectors*/
-
- 	*SCR = 6; /* Set the DEEPSLEEP bit and the SLEEPONEXIT bit  */
-
-    __asm("WFI"); /* Wait for interrupts */
-
+ 	*SCR = 6;
+  __asm("WFI");
 
   	while(1){}
 
@@ -51,5 +50,12 @@ int main(void) {
 
 void setupNVIC()
 {
+  /* TODO use the NVIC ISERx registers to enable handling of interrupt(s)
+     remember two things are necessary for interrupt handling:
+      - the peripheral must generate an interrupt signal
+      - the NVIC must be configured to make the CPU handle the signal
+     You will need TIMER1, GPIO odd and GPIO even interrupt handling for this
+     assignment.
+  */
 	 *ISER0 = 0x4000802;
 }
